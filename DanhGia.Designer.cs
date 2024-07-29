0cbc2410c0782d7580909ffdabfe4de244b1b4b6
adm <adm@HIEUPHAN>
2024-07-29 21:36:31 +0700

    aa

--- DanhGia.Designer.cs
+++ DanhGia.Designer.cs
@@ -28,112 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DanhGia));
             grpmenu = new GroupBox();
+            groupBox4 = new GroupBox();
+            txt_timkiem = new TextBox();
+            btn_timkiem = new Button();
             groupBox3 = new GroupBox();
-            pictureBox2 = new PictureBox();
-            label2 = new Label();
-            textBox3 = new TextBox();
+            btn_xoa = new Button();
+            btn_capnhat = new Button();
+            btn_Them = new Button();
             groupBox2 = new GroupBox();
-            pictureBox1 = new PictureBox();
-            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
             textBox2 = new TextBox();
+            label2 = new Label();
+            txt_tensach = new TextBox();
+            txt_masach = new TextBox();
+            lb_tensach = new Label();
+            lb_masach = new Label();
             grpmenu.SuspendLayout();
+            groupBox4.SuspendLayout();
             groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // grpmenu
             // 
+            grpmenu.Controls.Add(groupBox4);
             grpmenu.Controls.Add(groupBox3);
             grpmenu.Controls.Add(groupBox2);
+            grpmenu.Controls.Add(groupBox1);
             grpmenu.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            grpmenu.Location = new Point(110, 104);
+            grpmenu.Location = new Point(129, 149);
             grpmenu.Name = "grpmenu";
-            grpmenu.Size = new Size(1271, 689);
-            grpmenu.TabIndex = 20;
+            grpmenu.Size = new Size(1403, 616);
+            grpmenu.TabIndex = 26;
             grpmenu.TabStop = false;
-            grpmenu.Text = "Đánh Giá";
+            grpmenu.Text = "Thông tin đánh giá";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(txt_timkiem);
+            groupBox4.Controls.Add(btn_timkiem);
+            groupBox4.Location = new Point(556, 436);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(634, 77);
+            groupBox4.TabIndex = 19;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Tìm kiếm";
+            // 
+            // txt_timkiem
+            // 
+            txt_timkiem.Location = new Point(13, 33);
+            txt_timkiem.Name = "txt_timkiem";
+            txt_timkiem.Size = new Size(498, 27);
+            txt_timkiem.TabIndex = 12;
+            // 
+            // btn_timkiem
+            // 
+            btn_timkiem.BackColor = Color.White;
+            btn_timkiem.Location = new Point(545, 32);
+            btn_timkiem.Name = "btn_timkiem";
+            btn_timkiem.Size = new Size(79, 29);
+            btn_timkiem.TabIndex = 4;
+            btn_timkiem.Text = "Tìm kiếm";
+            btn_timkiem.UseVisualStyleBackColor = false;
             // 
             // groupBox3
             // 
-            groupBox3.Controls.Add(pictureBox2);
-            groupBox3.Controls.Add(label2);
-            groupBox3.Controls.Add(textBox3);
-            groupBox3.Location = new Point(572, 48);
+            groupBox3.Controls.Add(btn_xoa);
+            groupBox3.Controls.Add(btn_capnhat);
+            groupBox3.Controls.Add(btn_Them);
+            groupBox3.Location = new Point(110, 436);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(409, 388);
-            groupBox3.TabIndex = 1;
+            groupBox3.Size = new Size(340, 77);
+            groupBox3.TabIndex = 21;
             groupBox3.TabStop = false;
-            groupBox3.Text = "Chất lượng phục vụ";
+            groupBox3.Text = "Cập nhập";
             // 
-            // pictureBox2
+            // btn_xoa
             // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(0, 26);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(376, 180);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
+            btn_xoa.Location = new Point(212, 32);
+            btn_xoa.Name = "btn_xoa";
+            btn_xoa.Size = new Size(94, 29);
+            btn_xoa.TabIndex = 2;
+            btn_xoa.Text = "Xóa";
+            btn_xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_capnhat
+            // 
+            btn_capnhat.Location = new Point(112, 32);
+            btn_capnhat.Name = "btn_capnhat";
+            btn_capnhat.Size = new Size(94, 29);
+            btn_capnhat.TabIndex = 1;
+            btn_capnhat.Text = "Cập nhật";
+            btn_capnhat.UseVisualStyleBackColor = true;
+            // 
+            // btn_Them
+            // 
+            btn_Them.Location = new Point(12, 32);
+            btn_Them.Name = "btn_Them";
+            btn_Them.Size = new Size(94, 29);
+            btn_Them.TabIndex = 0;
+            btn_Them.Text = "Thêm";
+            btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(110, 214);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(1070, 215);
+            groupBox2.TabIndex = 20;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Thông tin";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(35, 21);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(996, 188);
+            dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txt_tensach);
+            groupBox1.Controls.Add(txt_masach);
+            groupBox1.Controls.Add(lb_tensach);
+            groupBox1.Controls.Add(lb_masach);
+            groupBox1.Location = new Point(110, 34);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1070, 174);
+            groupBox1.TabIndex = 18;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin đánh giá";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(761, 59);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(270, 27);
+            textBox2.TabIndex = 14;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(6, 219);
+            label2.Location = new Point(761, 36);
             label2.Name = "label2";
-            label2.Size = new Size(131, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Nhận xét của bạn";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Bình luận";
             // 
-            // textBox3
+            // txt_tensach
             // 
-            textBox3.Location = new Point(6, 252);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(382, 130);
-            textBox3.TabIndex = 1;
+            txt_tensach.Location = new Point(410, 59);
+            txt_tensach.Name = "txt_tensach";
+            txt_tensach.Size = new Size(296, 27);
+            txt_tensach.TabIndex = 8;
             // 
-            // groupBox2
+            // txt_masach
             // 
-            groupBox2.Controls.Add(pictureBox1);
-            groupBox2.Controls.Add(label1);
-            groupBox2.Controls.Add(textBox2);
-            groupBox2.Location = new Point(81, 48);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(418, 388);
-            groupBox2.TabIndex = 0;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "Chất lượng bữa ăn";
+            txt_masach.Location = new Point(69, 59);
+            txt_masach.Name = "txt_masach";
+            txt_masach.Size = new Size(287, 27);
+            txt_masach.TabIndex = 6;
             // 
-            // pictureBox1
+            // lb_tensach
             // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 26);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(376, 180);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
+            lb_tensach.AutoSize = true;
+            lb_tensach.Location = new Point(410, 36);
+            lb_tensach.Name = "lb_tensach";
+            lb_tensach.Size = new Size(71, 20);
+            lb_tensach.TabIndex = 2;
+            lb_tensach.Text = "Đánh giá";
             // 
-            // label1
+            // lb_masach
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(6, 219);
-            label1.Name = "label1";
-            label1.Size = new Size(131, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Nhận xét của bạn";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(6, 252);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(382, 130);
-            textBox2.TabIndex = 0;
+            lb_masach.AutoSize = true;
+            lb_masach.Location = new Point(69, 36);
+            lb_masach.Name = "lb_masach";
+            lb_masach.Size = new Size(118, 20);
+            lb_masach.TabIndex = 0;
+            lb_masach.Text = "Tên khách hàng";
             // 
             // DanhGia
             // 
@@ -145,25 +226,34 @@
             Name = "DanhGia";
             Text = "DanhGia";
             grpmenu.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
             groupBox3.ResumeLayout(false);
-            groupBox3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox grpmenu;
+        private GroupBox groupBox4;
+        private TextBox txt_timkiem;
+        private Button btn_timkiem;
         private GroupBox groupBox3;
-        private PictureBox pictureBox2;
-        private Label label2;
-        private TextBox textBox3;
+        private Button btn_xoa;
+        private Button btn_capnhat;
+        private Button btn_Them;
         private GroupBox groupBox2;
-        private PictureBox pictureBox1;
-        private Label label1;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
         private TextBox textBox2;
+        private Label label2;
+        private TextBox txt_tensach;
+        private TextBox txt_masach;
+        private Label lb_tensach;
+        private Label lb_masach;
     }
 }
